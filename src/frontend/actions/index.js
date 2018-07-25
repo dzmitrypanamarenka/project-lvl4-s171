60b57823756d75a0864bb57bfb8d9ace2d8e5687
Dzmitry Panamarenka <dzmitry_panamarenka@epam.com>
2018-07-25 17:33:17 +0300

    #typescript

--- src/frontend/actions/index.js
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash';
-import { createAction } from 'redux-actions';
-
-export const addTask = createAction('TASK_ADD', task =>
-  ({ task: { ...task, state: 'active', id: _.uniqueId() } }));
--- src/frontend/components/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // eslint-disable-line
-import NewTaskFormContainer from '../containers/TaskForm';
-
-export default () => (
-  <div className="col-5">
-    <NewTaskFormContainer />
-  </div>
-);
